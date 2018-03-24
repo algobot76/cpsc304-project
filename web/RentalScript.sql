74fd7281798163fdb6f89a3a8bbdd7caf3ea23d5
kristyyang1110 <kristyyang1110@gmail.com>
2018-03-24 11:47:23 -0700

    Make sql script work

--- web/RentalScript.sql
+++ web/RentalScript.sql
@@ -18,6 +18,8 @@
 --
 -- Table structure for table `PostalCode`
 --
+CREATE DATABASE RentalDatabase;
+USE RentalDatabase;
 
 DROP TABLE IF EXISTS `PostalCode`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;

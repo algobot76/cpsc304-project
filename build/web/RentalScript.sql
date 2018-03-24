f4731f3e41cd6a966b4fb29331127184e2c30c6c
Kaitian Xie <xkaitian@gmail.com>
2018-03-24 12:36:06 -0700

    Add Property

--- build/web/RentalScript.sql
+++ build/web/RentalScript.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `RentalDatabase` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `RentalDatabase`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: RentalDatabase
@@ -39,6 +41,31 @@ LOCK TABLES `PostalCode` WRITE;
 INSERT INTO `PostalCode` VALUES ('k1p 5m7','ottawa','on'),('t2e 6j8','calgary','ab');
 /*!40000 ALTER TABLE `PostalCode` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Property`
+--
+
+DROP TABLE IF EXISTS `Property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Property` (
+  `property_id` char(10) NOT NULL,
+  `address` char(50) NOT NULL,
+  `postal_code` char(7) DEFAULT NULL,
+  PRIMARY KEY (`property_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Property`
+--
+
+LOCK TABLES `Property` WRITE;
+/*!40000 ALTER TABLE `Property` DISABLE KEYS */;
+INSERT INTO `Property` VALUES ('100','a','b');
+/*!40000 ALTER TABLE `Property` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -49,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-24 11:36:36
+-- Dump completed on 2018-03-24 12:32:28

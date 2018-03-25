2d9e5c3d6a86dce74e429f642d3d36901a9c8f2c
kristyyang1110 <kristyyang1110@gmail.com>
2018-03-24 19:00:31 -0700

    add some forien keys

--- web/RentalScript.sql
+++ web/RentalScript.sql
@@ -1,5 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `RentalDatabase` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `RentalDatabase`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: 127.0.0.1    Database: RentalDatabase
+-- ------------------------------------------------------
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -11,6 +16,32 @@ USE `RentalDatabase`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Customer`
+--
+
+DROP TABLE IF EXISTS `Customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Customer` (
+  `phone` char(25) DEFAULT NULL,
+  `email` char(25) NOT NULL,
+  `name` char(50) DEFAULT NULL,
+  `customer_id` char(10) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customer`
+--
+
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `CustomerContactRealtor`
@@ -28,9 +59,39 @@ CREATE TABLE `CustomerContactRealtor` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `CustomerContactRealtor`
+--
+
 LOCK TABLES `CustomerContactRealtor` WRITE;
 /*!40000 ALTER TABLE `CustomerContactRealtor` DISABLE KEYS */;
 /*!40000 ALTER TABLE `CustomerContactRealtor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Feature`
+--
+
+DROP TABLE IF EXISTS `Feature`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Feature` (
+  `feature_name` char(50) NOT NULL,
+  `description` char(50) DEFAULT NULL,
+  `property_id` char(10) NOT NULL,
+  PRIMARY KEY (`feature_name`,`property_id`),
+  KEY `propertyid_idx` (`property_id`),
+  CONSTRAINT `propertyid` FOREIGN KEY (`property_id`) REFERENCES `Property` (`property_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Feature`
+--
+
+LOCK TABLES `Feature` WRITE;
+/*!40000 ALTER TABLE `Feature` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Feature` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,136 +109,13 @@ CREATE TABLE `ForRent` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `ForRent`
+--
+
 LOCK TABLES `ForRent` WRITE;
 /*!40000 ALTER TABLE `ForRent` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ForRent` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `PostalCode`
---
-
-DROP TABLE IF EXISTS `PostalCode`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `PostalCode` (
-  `postal_code` char(7) NOT NULL,
-  `city` char(50) DEFAULT NULL,
-  `province` char(2) DEFAULT NULL,
-  PRIMARY KEY (`postal_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `PostalCode` WRITE;
-/*!40000 ALTER TABLE `PostalCode` DISABLE KEYS */;
-/*!40000 ALTER TABLE `PostalCode` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Property`
---
-
-DROP TABLE IF EXISTS `Property`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Property` (
-  `property_id` char(10) NOT NULL,
-  `address` char(50) NOT NULL,
-  `postal_code` char(7) DEFAULT NULL,
-  `type` char(50) DEFAULT NULL,
-  `sq_ft` float DEFAULT NULL,
-  `date_built` datetime DEFAULT NULL,
-  `data_added` datetime DEFAULT NULL,
-  `num_beds` int(11) DEFAULT NULL,
-  `num_baths` float DEFAULT NULL,
-  PRIMARY KEY (`property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `Property` WRITE;
-/*!40000 ALTER TABLE `Property` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Property` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Rented`
---
-
-DROP TABLE IF EXISTS `Rented`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Rented` (
-  `property_id` char(10) NOT NULL,
-  `final_rent` int(11) DEFAULT NULL,
-  `from_date` datetime DEFAULT NULL,
-  `to_date` datetime DEFAULT NULL,
-  `customer_id` char(10) NOT NULL,
-  PRIMARY KEY (`property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `Rented` WRITE;
-/*!40000 ALTER TABLE `Rented` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Rented` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Room`
---
-
-DROP TABLE IF EXISTS `Room`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Room` (
-  `room_name` char(50) NOT NULL,
-  `property_id` char(10) NOT NULL,
-  `image_url` text,
-  PRIMARY KEY (`room_name`,`property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `Room` WRITE;
-/*!40000 ALTER TABLE `Room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Room` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-DROP TABLE IF EXISTS `Customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Customer` (
-  `phone` char(25) DEFAULT NULL,
-  `email` char(25) NOT NULL,
-  `name` char(50) DEFAULT NULL,
-  `customer_id` char(10) NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `Customer` WRITE;
-/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Feature`
---
-
-DROP TABLE IF EXISTS `Feature`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Feature` (
-  `feature_name` char(50) NOT NULL,
-  `description` char(50) DEFAULT NULL,
-  `property_id` char(10) NOT NULL,
-  PRIMARY KEY (`feature_name`,`property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-LOCK TABLES `Feature` WRITE;
-/*!40000 ALTER TABLE `Feature` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Feature` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -190,9 +128,14 @@ DROP TABLE IF EXISTS `ForSale`;
 CREATE TABLE `ForSale` (
   `property_id` char(10) NOT NULL,
   `price` int(11) DEFAULT NULL,
-  PRIMARY KEY (`property_id`)
+  PRIMARY KEY (`property_id`),
+  CONSTRAINT `ppid` FOREIGN KEY (`property_id`) REFERENCES `Property` (`property_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ForSale`
+--
 
 LOCK TABLES `ForSale` WRITE;
 /*!40000 ALTER TABLE `ForSale` DISABLE KEYS */;
@@ -214,11 +157,19 @@ CREATE TABLE `PostalCode` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `PostalCode`
+--
+
 LOCK TABLES `PostalCode` WRITE;
 /*!40000 ALTER TABLE `PostalCode` DISABLE KEYS */;
 INSERT INTO `PostalCode` VALUES ('k1p 5m7','ottawa','on'),('t2e 6j8','calgary','ab');
 /*!40000 ALTER TABLE `PostalCode` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Property`
+--
 
 DROP TABLE IF EXISTS `Property`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -227,15 +178,25 @@ CREATE TABLE `Property` (
   `property_id` char(10) NOT NULL,
   `address` char(50) NOT NULL,
   `postal_code` char(7) DEFAULT NULL,
-  PRIMARY KEY (`property_id`)
+  `realtor_id` char(10) NOT NULL,
+  PRIMARY KEY (`property_id`),
+  KEY `property1_idx` (`realtor_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Property`
+--
+
 LOCK TABLES `Property` WRITE;
 /*!40000 ALTER TABLE `Property` DISABLE KEYS */;
-INSERT INTO `Property` VALUES ('100','a','b');
+INSERT INTO `Property` VALUES ('100','a','b','');
 /*!40000 ALTER TABLE `Property` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Realtor`
+--
 
 DROP TABLE IF EXISTS `Realtor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -247,14 +208,24 @@ CREATE TABLE `Realtor` (
   `realtor_id` char(10) NOT NULL,
   `branch_id` char(10) NOT NULL,
   PRIMARY KEY (`realtor_id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `branchid_idx` (`branch_id`),
+  CONSTRAINT `branchid` FOREIGN KEY (`branch_id`) REFERENCES `RealtyOffice` (`branch_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Realtor`
+--
 
 LOCK TABLES `Realtor` WRITE;
 /*!40000 ALTER TABLE `Realtor` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Realtor` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `RealtyOffice`
+--
 
 DROP TABLE IF EXISTS `RealtyOffice`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -264,14 +235,74 @@ CREATE TABLE `RealtyOffice` (
   `branch_name` char(50) DEFAULT NULL,
   `address` char(50) DEFAULT NULL,
   `postal_code` char(50) DEFAULT NULL,
-  PRIMARY KEY (`branch_id`)
+  PRIMARY KEY (`branch_id`),
+  KEY `postalcode_idx` (`postal_code`),
+  CONSTRAINT `postalcode` FOREIGN KEY (`postal_code`) REFERENCES `PostalCode` (`postal_code`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `RealtyOffice`
+--
 
 LOCK TABLES `RealtyOffice` WRITE;
 /*!40000 ALTER TABLE `RealtyOffice` DISABLE KEYS */;
 /*!40000 ALTER TABLE `RealtyOffice` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Rented`
+--
+
+DROP TABLE IF EXISTS `Rented`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Rented` (
+  `property_id` char(10) NOT NULL,
+  `final_rent` int(11) DEFAULT NULL,
+  `from_date` datetime DEFAULT NULL,
+  `to_date` datetime DEFAULT NULL,
+  `customer_id` char(10) NOT NULL,
+  PRIMARY KEY (`property_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rented`
+--
+
+LOCK TABLES `Rented` WRITE;
+/*!40000 ALTER TABLE `Rented` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Rented` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Room`
+--
+
+DROP TABLE IF EXISTS `Room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Room` (
+  `room_name` char(50) NOT NULL,
+  `property_id` char(10) NOT NULL,
+  `image_url` text,
+  PRIMARY KEY (`room_name`,`property_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Room`
+--
+
+LOCK TABLES `Room` WRITE;
+/*!40000 ALTER TABLE `Room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Sold`
+--
 
 DROP TABLE IF EXISTS `Sold`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -281,15 +312,23 @@ CREATE TABLE `Sold` (
   `final_price` int(11) DEFAULT NULL,
   `date_sold` datetime DEFAULT NULL,
   `customer_id` char(10) DEFAULT NULL,
-  PRIMARY KEY (`property_id`)
+  PRIMARY KEY (`property_id`),
+  KEY `ccid_idx` (`customer_id`),
+  CONSTRAINT `sold1` FOREIGN KEY (`property_id`) REFERENCES `Property` (`property_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sold2` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`customer_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Sold`
+--
 
 LOCK TABLES `Sold` WRITE;
 /*!40000 ALTER TABLE `Sold` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Sold` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -297,3 +336,5 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-24 19:00:08
